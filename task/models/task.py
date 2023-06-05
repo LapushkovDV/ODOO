658f613fe8e978bce2476de326708775758d9cf2
IvanAndriukhin <ivan.andriukhin@gmail.com>
2023-06-05 12:31:29 +0300

    Event task views

--- task/models/task.py
+++ task/models/task.py
@@ -43,6 +43,14 @@ class Task(models.Model):
     subtask_count = fields.Integer("Sub-task Count", compute='_compute_subtask_count')
     child_text = fields.Char(compute="_compute_child_text")
     is_closed = fields.Boolean(string="Closed", index=True)
+    attachment_count = fields.Integer(compute='_compute_attachment_count', string='Documents')
+
+    def _compute_attachment_count(self):
+        for task in self:
+            task.attachment_count = self.env['ir.attachment'].search_count([
+                ('res_model', '=', type(task).__name__),
+                ('res_id', '=', task.id)
+            ])
 
     @api.model
     def _selection_parent_model(self):
@@ -134,6 +142,20 @@ class Task(models.Model):
             'res_id': self.parent_id.id,
             'type': 'ir.actions.act_window',
             'context': self._context
+        }
+
+    def action_open_attachments(self):
+        self.ensure_one()
+        return {
+            'name': _('Attachments'),
+            'domain': [('res_model', '=', self._name), ('res_id', '=', self.id)],
+            'res_model': 'ir.attachment',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'kanban,tree,form',
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % (self._name, self.id),
+            'help': """
+                <p class="o_view_nocontent_smiling_face">%s</p>
+                """ % _("Add attachments for this task")
         }
 
 
