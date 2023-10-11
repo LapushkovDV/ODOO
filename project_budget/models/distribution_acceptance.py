8d7c8c61d9e5eea8f1eac7f84f996c66eb4198de
IliaYusov <ouster2k@gmail.com>
2023-10-11 12:55:28 +0300

    forecasts for distributions

--- project_budget/models/distribution_acceptance.py
+++ project_budget/models/distribution_acceptance.py
@@ -1,8 +1,8 @@
-
 from odoo import _, models, fields, api
 from odoo.exceptions import ValidationError
 from odoo.tools import pytz
 from datetime import timedelta
+
 
 class distribution_acceptance(models.Model):
     _name = 'project_budget.distribution_acceptance'
@@ -36,9 +36,9 @@ class distribution_acceptance(models.Model):
         related='planned_acceptance_flow_id.distribution_sum_with_vat_ostatok',
         string="distribution_sum_with_vat_ostatok", readonly=True)
 
-    sum_cash_without_vat = fields.Monetary(string="fact sum_cash_without_vat", required=True, copy=True)
+    sum_cash_without_vat = fields.Monetary(string="fact sum_cash_without_vat", compute='_compute_default_sum',
+                                           inverse='_inverse_compute_default_sum', store=True, required=True, copy=True)
     sum_cash = fields.Monetary(string="fact sum_cash", required=True, copy=True,compute='_compute_sum', readonly=True)
-
 
     @api.depends("sum_cash_without_vat")
     def _compute_sum(self):
@@ -48,3 +48,17 @@ class distribution_acceptance(models.Model):
 
             else:
                 row.write({'sum_cash': row.sum_cash_without_vat * (1 + row.fact_acceptance_flow_id.projects_id.vat_attribute_id.percent / 100)})
+
+    @api.depends("distribution_sum_without_vat_ostatok")
+    def _compute_default_sum(self):
+        for row in self:
+            distribution = {}
+            distr_list = [distribution for distribution in row.fact_acceptance_flow_id.distribution_acceptance_ids if 'virtual' in str(distribution.id)]
+            for distr in distr_list[:-1]:
+                distribution['total'] = distribution.get('total', 0) + distr.sum_cash_without_vat
+                distribution[distr.planned_acceptance_flow_id] = distribution.get(distr.planned_acceptance_flow_id, 0) + distr.sum_cash_without_vat
+            row.sum_cash_without_vat = min(row.distribution_sum_without_vat_ostatok - distribution.get(row.planned_acceptance_flow_id, 0), row.fact_acceptance_flow_id.sum_cash_without_vat - distribution.get('total', 0))
+
+
+    def _inverse_compute_default_sum(self):
+        pass
