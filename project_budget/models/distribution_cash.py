1218dcc7381dfaa5c4a546646e59af03f232b0ad
lapushkov <lapushkov@yandex.ru>
2023-11-04 09:38:02 +0300

    Убрал автоматический расчет дефолтной суммы - он косячит, плять писец как:
    при фиксации бюджета начинается пересчет сумм в распределении и суммы все покорежило хер знает как

--- project_budget/models/distribution_cash.py
+++ project_budget/models/distribution_cash.py
@@ -27,8 +27,8 @@ class distribution_cash(models.Model):
     distribution_sum_with_vat_ostatok = fields.Monetary(related='planned_cash_flow_id.distribution_sum_with_vat_ostatok', string ="distribution_sum_with_vat_ostatok", readonly=True)
 
     sum_cash_without_vat = fields.Monetary(string="fact sum_cash_without_vat", required=True, copy=True, compute='_compute_sum')
-    sum_cash = fields.Monetary(string="fact sum_cash", compute='_compute_default_sum',
-                                           inverse='_inverse_compute_default_sum', store=True, required=True, copy=True)
+    sum_cash = fields.Monetary(string="fact sum_cash", store=True, required=True, copy=True)
+    # compute = '_compute_default_sum', inverse = '_inverse_compute_default_sum',
 
     @api.depends("sum_cash")
     def _compute_sum(self):
@@ -39,17 +39,17 @@ class distribution_cash(models.Model):
                 row.sum_cash_without_vat = row.sum_cash / (1 + row.fact_cash_flow_id.projects_id.vat_attribute_id.percent / 100)
         # row.planned_cash_flow_id.compute_distribution_sum()
 
-    @api.depends("distribution_sum_with_vat_ostatok")
-    def _compute_default_sum(self):
-        for row in self:
-            distribution = {}
-            print(row.fact_cash_flow_id.distribution_cash_ids)
-            distr_list = [distribution for distribution in row.fact_cash_flow_id.distribution_cash_ids if hasattr(distribution.id, 'origin') and distribution.id.origin is not False]
-            for distr in distr_list[:-1]:
-                distribution['total'] = distribution.get('total', 0) + distr.sum_cash
-                if distr.id.origin is None:
-                    distribution[distr.planned_cash_flow_id] = distribution.get(distr.planned_cash_flow_id, 0) + distr.sum_cash
-            row.sum_cash = min(row.distribution_sum_with_vat_ostatok - distribution.get(row.planned_cash_flow_id, 0), row.fact_cash_flow_id.sum_cash - distribution.get('total', 0))
-
-    def _inverse_compute_default_sum(self):
-        pass
+    # @api.depends("distribution_sum_with_vat_ostatok")
+    # def _compute_default_sum(self):
+    #     for row in self:
+    #         distribution = {}
+    #         print(row.fact_cash_flow_id.distribution_cash_ids)
+    #         distr_list = [distribution for distribution in row.fact_cash_flow_id.distribution_cash_ids if hasattr(distribution.id, 'origin') and distribution.id.origin is not False]
+    #         for distr in distr_list[:-1]:
+    #             distribution['total'] = distribution.get('total', 0) + distr.sum_cash
+    #             if distr.id.origin is None:
+    #                 distribution[distr.planned_cash_flow_id] = distribution.get(distr.planned_cash_flow_id, 0) + distr.sum_cash
+    #         row.sum_cash = min(row.distribution_sum_with_vat_ostatok - distribution.get(row.planned_cash_flow_id, 0), row.fact_cash_flow_id.sum_cash - distribution.get('total', 0))
+    #
+    # def _inverse_compute_default_sum(self):
+    #     pass
