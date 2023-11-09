2697bb346c83f1bda9a0d927219bc335f59a6bea
IliaYusov <ouster2k@gmail.com>
2023-11-09 15:56:48 +0300

    dynamical names in MC report

--- project_budget/wizard/report_projects_wizard.py
+++ project_budget/wizard/report_projects_wizard.py
@@ -1,7 +1,8 @@
 from odoo import models, fields, _
 from odoo.exceptions import UserError
 from io import BytesIO
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+import pytz
 
 class report_projects_wizard(models.TransientModel):
     _name = 'project_budget.projects.report.wizard'
@@ -71,7 +72,13 @@ class report_projects_wizard(models.TransientModel):
             return self.env.ref('project_budget.action_projects_list_report_xlsx_overdue').report_action(self, data=datas)
 
         if self.type_report == 'management_committee':
-            return self.env.ref('project_budget.action_projects_list_report_xlsx_management_committee').report_action(self, data=datas)
+            report_obj = self.env.ref('project_budget.action_projects_list_report_xlsx_management_committee')
+            # name = {"en_US": "Projects Report Management Committee", "ru_RU": "Отчет для УК"}  # на память
+            companies = ''
+            for company_id in self.env.context.get('allowed_company_ids', ''):
+                companies += self.env['res.company'].search([('id', '=', company_id)]).name.strip().replace(' ', '_') + '_'
+            report_obj.sudo().name = f"{companies}_{datetime.now(pytz.timezone('Europe/Moscow')).strftime('%Y%m%d_%H%M%S')}"
+            return report_obj.report_action(self, data=datas)
 
         if self.type_report == 'pds_acceptance_by_date':
             return self.env.ref('project_budget.action_projects_list_report_xlsx_pds_acceptance_by_date').report_action(self, data=datas)
