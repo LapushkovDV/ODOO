8e5f428e078d6c05628ff8b1ee1ca5bfb7720ca3
IvanAndriukhin <ivan.andriukhin@gmail.com>
2023-11-11 00:55:00 +0300

    Убираем апдейт ir_report для формирования наименование отчета УК

--- project_budget/wizard/report_projects_wizard.py
+++ project_budget/wizard/report_projects_wizard.py
@@ -74,14 +74,10 @@ class report_projects_wizard(models.TransientModel):
             return self.env.ref('project_budget.action_projects_list_report_xlsx_overdue').report_action(self, data=datas)
 
         if self.type_report == 'management_committee':
-            # return self.env.ref('project_budget.action_projects_list_report_xlsx_management_committee').report_action(self, data=datas)
-            report_obj = self.env.ref('project_budget.action_projects_list_report_xlsx_management_committee')
-            # name = {"en_US": "Projects Report Management Committee", "ru_RU": "Отчет для УК"}  # на память
-            companies = ''
-            for company_id in self.env.context.get('allowed_company_ids', ''):
-                companies += self.env['res.company'].search([('id', '=', company_id)]).name.strip().replace(' ', '_') + '_'
-            report_obj.sudo().name = f"{companies}_{datetime.now(pytz.timezone('Europe/Moscow')).strftime('%Y%m%d_%H%M%S')}"
-            return report_obj.report_action(self, data=datas)
+            c_ids = self.env['res.company'].browse(self.env.context.get('allowed_company_ids', []))
+            print_report_name = f"{'_'.join(name.strip().replace(' ', '_') for name in c_ids.mapped('name'))}_{datetime.now(pytz.timezone(self.env.user.tz)).strftime('%Y%m%d_%H%M%S')}"
+            return self.env.ref('project_budget.action_projects_list_report_xlsx_management_committee').with_context(
+                print_report_name=print_report_name).report_action(self, data=datas)
         
         if self.type_report == 'pds_acceptance_by_date':
             return self.env.ref('project_budget.action_projects_list_report_xlsx_pds_acceptance_by_date').report_action(self, data=datas)
