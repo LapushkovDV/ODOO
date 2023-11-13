17da7ea1b23a6adc7905e946cb059df2fed2eaa2
lapushkov <lapushkov@yandex.ru>
2023-11-13 09:59:59 +0300

    корректировка создания дурнальной таблицы: убираем ограничения NotNull

--- project_budget/models/test.py
+++ project_budget/models/test.py
@@ -1,33 +1,12 @@
-dict21 = {
-            'response':
-                        [{'id': 42565717,
-                           'name': 'Python',
-                           'screen_name': 'club42565717',
-                           'is_closed': 0,
-                           'type': 'group',
-                           'members_count': 37319,
-                           'activity': 'Открытая группа',
-                           'photo_50': 'https://sun9-127.userapi.com/c845524/v845524906/1a71c2/A2r_4JtmiLQ.jpg?ava=1',
-                           'photo_100': 'https://sun9-58.userapi.com/c845524/v845524906/1a71c1/2fBtsS0k8XY.jpg?ava=1',
-                           'photo_200': 'https://sun9-50.userapi.com/c845524/v845524906/1a71c0/Kfo-eQIn0DU.jpg?ava=1'
-                          },
-                          {'id': 3183750,
-                           'name': 'Веб программист - PHP, JS, Python, Java, HTML 5',
-                           'screen_name': 'php2all',
-                           'is_closed': 0,
-                           'type': 'page',
-                           'members_count': 117833,
-                           'activity': 'Программирование',
-                           'photo_50': 'https://sun9-54.userapi.com/c626421/v626421613/941/HSj4ylRsk8k.jpg?ava=1',
-                           'photo_100': 'https://sun9-5.userapi.com/c626421/v626421613/940/yKaZLxGShkY.jpg?ava=1',
-                           'photo_200': 'https://sun9-49.userapi.com/c626421/v626421613/93f/2EygT_FJKWg.jpg?ava=1'
-                           }
-                        ],
-            'status': 'OK',
-            'data_type': 'JSON',
-            'data_type2': 'JSON RPC',
-         }
+a = 1
+b = 1
+i = 2
+n = 9
+fibonacci_list = [1,1]
+while i < n:
 
-a = dict21.values()
-
-print(a)
+    b, a = a + b, b
+    fibonacci_list.append(b)
+    i += 1
+    print(i)
+print(fibonacci_list)
