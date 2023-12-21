b01851f6244431bc61d0991ae20f8e892029c665
IliaYusov <ouster2k@gmail.com>
2023-12-21 10:05:57 +0300

    new theme

--- base_custom_app/__manifest__.py
+++ base_custom_app/__manifest__.py
@@ -1,20 +1,36 @@
 {
     'name': 'Customize ODOO',
-    'version': '1.0.3',
+    'version': '2.0.0',
     'depends': ['base', 'web'],
     'category': 'Productivity',
     'description': """
         Customize ODOO application
     """,
     'assets': {
+        'web._assets_primary_variables': [
+            'base_custom_app/static/src/scss/primary_variables_custom.scss',
+        ],
         'web.assets_backend': [
             'base_custom_app/static/src/webclient/webclient.js',
-            'base_custom_app/static/src/scss/change_color.scss',
             'base_custom_app/static/src/js/set_width.js',
             'base_custom_app/static/src/js/set_title.js',
             'base_custom_app/static/src/js/user_menu.js',
-            'base_custom_app/static/src/scss/form_view_extra.scss',
-            'base_custom_app/static/src/scss/sticky_header.scss'
+            'base_custom_app/static/src/scss/new_layout.scss',
+            'base_custom_app/static/src/css/main.css',
+            'base_custom_app/static/src/css/navbar.css',
+            'base_custom_app/static/src/css/header.css',
+            'base_custom_app/static/src/css/form.css',
+            'base_custom_app/static/src/css/list.css',
+            'base_custom_app/static/src/css/kanban.css',
+            'base_custom_app/static/src/css/calendar.css',
+            'base_custom_app/static/src/css/pivot.css',
+            'base_custom_app/static/src/css/graph.css',
+            'base_custom_app/static/src/css/mail.css',
+            'base_custom_app/static/src/css/activity.css',
+            'base_custom_app/static/src/css/card.css',
+            'base_custom_app/static/src/css/dashboard.css',
+            'base_custom_app/static/src/css/chatter.css',
+            'base_custom_app/static/src/css/other.css',
         ]
     },
     'data': [
