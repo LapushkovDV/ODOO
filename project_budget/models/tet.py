7d3dcf55e661c667ce6d46f077619e99bf267bf0
lapushkov <lapushkov@yandex.ru>
2024-01-10 12:35:45 +0300

    test pandas

--- project_budget/models/tet.py
+++ project_budget/models/tet.py
@@ -1,3 +1,15 @@
-a = 7
-b = 9+a
-print("a=F(",b,")")
+import pandas as pd
+def create_medications(names, counts):
+    medications = pd.Series(data=[*counts],index=[*names])
+    return medications
+def get_percent(medications, name):
+    percent = medications.loc[name]/sum(medications) * 100
+    return percent
+
+
+names=['chlorhexidine', 'cyntomycin', 'afobazol']
+counts=[15, 18, 7]
+
+print(get_percent(create_medications(names, counts), "chlorhexidine")) #37.5
+
+
