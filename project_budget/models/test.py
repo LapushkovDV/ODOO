6d15e113026664c5a1c089c1de912a8f15d11cf4
lapushkov <lapushkov@yandex.ru>
2024-02-25 09:04:15 +0300

    triafly коннектор

--- project_budget/models/test.py
+++ project_budget/models/test.py
@@ -1,7 +1,17 @@
-import numpy as np
+import requests
 
-seed = np.random.uniform(0,2**32-1)
-seed = int(seed // 1)
-print(seed)
-print(seed)
-print(seed)
+triaflyURL = 'http://194.169.192.155:55556'
+triaflyAPI_KEY = '8EBEA456a6'
+triaflyUser = 'admin'
+triaflyPswd = 'Zraeqw123'
+triaflyRegistr = '471971'
+
+_rqst = '{0}/api/3/registry/{1}/'.format(triaflyURL,triaflyRegistr)
+
+print(_rqst)
+_resRqst = requests.get(_rqst,
+    params={'user': '{0}:{1}'.format(triaflyUser, triaflyPswd)},
+    headers={'Netdb-Api-Key': '8EBEA456a6'}
+                        )
+print(_resRqst.json())
+print(_resRqst)
