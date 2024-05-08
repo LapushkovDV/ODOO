be0e007dc897a2f2ce74d458bd42d72711f16c8b
IvanAndriukhin <ivan.andriukhin@gmail.com>
2024-05-08 16:36:08 +0300

    Подключение справочника партнеров к хранилищу документов

---
+++ dms_company_partner/models/res_company_partner.py
@@ -0,0 +1,49 @@
+from odoo import api, fields, models, _
+
+
+class ResCompanyPartner(models.Model):
+    _name = 'res.company.partner'
+    _inherit = ['res.company.partner', 'dms.document.mixin']
+
+    directory_id = fields.Many2one('dms.directory', compute='_compute_directory_id')
+
+    # ------------------------------------------------------
+    # DMS.DOCUMENT.MIXIN
+    # ------------------------------------------------------
+
+    def _get_document_directory(self):
+        return self.env.ref('dms_company_partner.dms_directory_company_partner_directory')
+
+    def _get_document_partner(self):
+        return self.partner_id
+
+    # ------------------------------------------------------
+    # COMPUTE METHODS
+    # ------------------------------------------------------
+
+    def _compute_directory_id(self):
+        for rec in self:
+            rec.directory_id = self.env.ref('dms_company_partner.dms_directory_company_partner_directory')
+
+    # ------------------------------------------------------
+    # CRUD
+    # ------------------------------------------------------
+
+    def write(self, vals):
+        res = super(ResCompanyPartner, self).write(vals)
+        if res and vals.get('partner_id'):
+            [rec._move_documents_to_partner() for rec in self]
+        return res
+
+    # ------------------------------------------------------
+    # PRIVATE METHODS
+    # ------------------------------------------------------
+
+    def _move_documents_to_partner(self):
+        documents = self.env['dms.document'].sudo().search([
+            ('res_model', '=', self._name),
+            ('res_id', '=', self.id),
+            ('partner_id', '!=', self.partner_id.id)
+        ])
+        if documents:
+            documents.write({'partner_id': self.partner_id.id})
