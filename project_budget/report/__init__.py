d91d5e442ff843216d08843000abc56eb10f3e27
IvanAndriukhin <ivan.andriukhin@gmail.com>
2024-05-29 11:28:49 +0300

    Разрешение редактирование сделок (зафиксированных, рабочих) администраторам модуля, связанный рефакторинг представления

--- project_budget/report/__init__.py
+++ project_budget/report/__init__.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from . import project_budget_report_excel
-from . import project_budget_forecast_report_excel
 from . import project_budget_forecast_v2_report_excel
 from . import project_budget_plan_fact_report_excel
 from . import report_svod_excel
