545814719266a8c024ffaa7c23af6200ff78e641
IvanAndriukhin <ivan.andriukhin@gmail.com>
2024-06-13 21:33:50 +0300

    Инициализация РМ модуля управления проектами

---
+++ workplace_employee_project_budget/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Workplace Employee: Project Budget',
+    'version': '1.0.0',
+    'category': '',
+    'depends': ['workplace_employee', 'project_budget'],
+    'description': """
+    """,
+    'author': '',
+    'support': '',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+        'web.assets_backend': [
+            'workplace_employee_project_budget/static/src/components/**/*.js',
+            'workplace_employee_project_budget/static/src/components/**/*.xml',
+            'workplace_employee_project_budget/static/src/css/workplace_employee_project_budget_dashboard.css'
+        ]
+    },
+    'license': 'LGPL-3'
+}
