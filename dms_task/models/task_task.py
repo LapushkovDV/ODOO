79724a40b8268f5e8d52215cbde6a4b2a4b67e4f
IvanAndriukhin <ivan.andriukhin@gmail.com>
2024-07-11 12:49:28 +0300

    Модуль, забытый при основной миграции

--- dms_task/models/task_task.py
+++ dms_task/models/task_task.py
@@ -9,8 +9,8 @@ class Task(models.Model):
     @api.depends('parent_ref', 'parent_ref_type', 'parent_ref_id')
     def _compute_file_ids(self):
         for task in self:
-            task.file_ids = task.parent_ref.document_ids if task.parent_ref and task.parent_ref._fields.get(
-                'document_ids') else False
+            task.file_ids = task.parent_ref.file_ids if task.parent_ref and task.parent_ref._fields.get(
+                'file_ids') else False
 
     def _get_attachment_ids_for_email(self):
         return self.execution_result_attachment_ids.ids if self.is_closed else self.file_ids.attachment_id.ids
